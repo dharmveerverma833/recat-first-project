a729cc27a36ed3cc49a20ff69ec5a28cdd3d9cc0
dharmveerverma833 <dharmveerverma894@gmail.com>
2026-01-10 04:41:49 +0530

    change

---
+++ front-end/src/App.jsx
@@ -0,0 +1,15 @@
+
+import Chai from './chai.jsx'
+
+
+function App() {  
+  return (
+    <>
+      <Chai/>
+      <h1>Hello World !</h1>
+      <h4>test pepar</h4>
+    </>
+  )
+}
+
+export default App;
---
+++ front-end/src/chai.jsx
@@ -0,0 +1,9 @@
+import { useState } from 'react'
+
+function Chai(){
+    return(
+    <h3>Dharm Veer verma</h3>
+    )
+
+}
+export default Chai
